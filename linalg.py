49b7659ad14df35bda9d6dc7790d35d9c6e5a7c6
sruthi-biju <88966525+sruthi-biju@users.noreply.github.com>
2022-01-23 17:24:41 +0530

    Updated transpose function
    Generalized the transpose function

--- linalg.py
+++ linalg.py
@@ -3,11 +3,14 @@ class Matrix():
         self.grid = [[aa, ab], [ba, bb]]
         self.determinant = aa * bb - ab * ba
     def transpose(self):
-        aa = self.grid[0][0]
-        ab = self.grid[0][1]
-        ba = self.grid[1][0]
-        bb = self.grid[1][1]
-        self.grid = [[aa, ba], [ab, bb]]
+        buffer = self.grid
+        new_grid =[]
+        for i in range(self.order[1]):
+          new_grid.append([])
+          for j in range(self.order[0]):
+            new_grid[i].append(buffer[j][i])
+        self.grid = new_grid
+        self.order = (self.order[1],self.order[0])
     def inverse(self):
         d = self.determinant
         if d != 0:
